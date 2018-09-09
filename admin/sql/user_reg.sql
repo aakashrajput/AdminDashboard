24783f1c750c351899cf6f3a80b8db1bc88fd0aa
aakashrajput <aakashsingh1999@gmail.com>
2018-09-09 23:55:06 +0530

    added user_reg tables in sql

---
+++ admin/sql/user_reg.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_reg` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

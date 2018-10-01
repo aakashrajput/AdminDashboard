e15e74309d39bb9e03bb41270246e3fa6c11e0a5
aakashrajput <aakashsingh1999@gmail.com>
2018-10-01 14:02:14 +0530

    Modified ongoing projects

--- employee/landingpage/ongoing_projects.php
+++ employee/landingpage/ongoing_projects.php
@@ -11,24 +11,24 @@ include "bluf_connection.php"
                               <div class="card-body table-full-width table-responsive">
                                   <table class="table table-hover">
                                     <?php
-                                    $res = mysqli_query($link,"select * from active_projects ORDER BY id DESC LIMIT 4");
+                                    $res = mysqli_query($link,"select * from assigned_project");
                                       echo "<thead>";
+                                          echo "<th>";echo "Project Name";echo "</th>";
                                           echo "<th>";echo "Team Leader";echo "</th>";
                                           echo "<th>";echo "Total Team Members";echo "</th>";
-                                          echo "<th>";echo "Project Name";echo "</th>";
                                           echo "<th>";echo "Assigned Date";echo "</th>";
-                                          echo "<th>";echo "Expected Completion Date";echo "</th>";
+                                          echo "<th>";echo "Expected Completed Date";echo "</th>";
                                           echo "<th>";echo "Assigned By";echo "</th>";
                                       echo "</thead>";
                                       while($row=mysqli_fetch_array($res))
                                 {
                                       echo "<tbody>";
                                           echo "<tr>";
-                                              echo"<td style='color:green;'>";echo $row["pr_leader"]; echo"</td>";
-                                              echo"<td>";echo $row["pr_members"]; echo"</td>";
-                                              echo"<td>";echo $row["pr_name"]; echo"</td>";
-                                              echo"<td>";echo $row["pr_date"]; echo"</td>";
-                                              echo"<td>";echo $row["pr_cdate"]; echo"</td>";
+                                              echo"<td style='color:green;'>";echo $row["sl_project"]; echo"</td>";
+                                              echo"<td>";echo $row["sl_projectl"]; echo"</td>";
+                                              echo"<td>";echo $row["sl_members"]; echo"</td>";
+                                              echo"<td>";echo $row["sl_date"]; echo"</td>";
+                                              echo"<td>";echo $row["sl_cdate"]; echo"</td>";
                                               echo"<td>";echo $row["username"]; echo"</td>";
                                           echo "</tr>";
                                       echo "</tbody>";
